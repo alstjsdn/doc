f816e69791528a3ba17302f8e05d55b5d17217c5
minsunwoo <swmin@dhicc.com>
2025-04-11 14:17:20 +0900

    commit

---
+++ src/main/java/com/doc/doctest/DoctestApplication.java
@@ -0,0 +1,13 @@
+package com.doc.doctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doc/doctest/DoctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.doctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
